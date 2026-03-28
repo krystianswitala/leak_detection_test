b479a5556f2b51c52b086326b85b4541d16e483c
Krystian Świtała <krystianswitala@gmail.com>
2026-03-28 19:25:04 +0100

    Refactor memory allocation and debug settings in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
-#include <iostream>
-#include <cstdint>
+#if defined(_DEBUG)
+#define _CRTDBG_MAP_ALLOC
 #include <crtdbg.h>
+#include <cstdlib>
 
-uint8_t *global_ptr = nullptr;
+#define new new (_NORMAL_BLOCK, __FILE__, __LINE__)
+#endif // _DEBUG
 
-int main(int, char**) {
+#include <cstdint>
+#include <iostream>
+
+uint8_t* global_ptr = nullptr;
+
+int main(int, char**)
+{
 #if defined(_DEBUG)
     // Enable run-time memory leak check for debug builds.
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
 #endif // _DEBUG
 
     std::cout << "Hello, from leak_detection_test!\n";
@@ -15,6 +24,16 @@ int main(int, char**) {
     global_ptr = new uint8_t[1024];
     for (size_t i = 0; i < 1024; ++i) {
         global_ptr[i] = static_cast<uint8_t>(i % 256);
+    }
+
+    uint8_t* local_ptr = new uint8_t[512];
+    for (size_t i = 0; i < 512; ++i) {
+        local_ptr[i] = static_cast<uint8_t>(i % 256);
+    }
+
+    uint8_t* another_local_ptr = static_cast<uint8_t*>(malloc(256));
+    for (size_t i = 0; i < 256; ++i) {
+        another_local_ptr[i] = static_cast<uint8_t>(i % 256);
     }
 
     return 0;
